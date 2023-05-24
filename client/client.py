02f5903d06f9bdcea3bcb8e35777f094e9d24006
Rankenstein <zphillip98@gmail.com>
2023-05-24 13:28:20 +0200

    Debugging CLI Client

--- client/client.py
+++ client/client.py
@@ -1,4 +1,35 @@
-import requests, jsonpickle
-response = requests.get("http://192.168.6.93:8080")
-print(response)
-print(jsonpickle.decode(response.text))
+import jsonpickle
+import requests
+
+from data.post import ClientPost
+
+USERID = -1
+while True:
+    user_in = input("r - Gibt neuesten Post aus; p - sendet einen Post\n")
+    match user_in:
+        case 'r':
+            response = None
+            try:
+                response = requests.get("http://192.168.6.93:8080")
+            except requests.ConnectionError as err:
+                print('Server Error:\n' + err)
+                continue
+            if response.status_code == 200:
+                post = jsonpickle.decode(response.text)
+                print(f'{post.userID}\n{post.text}\nLikes: {post.likes}')
+            else:
+                print('ErrorStatusCode = ', response.status_code)
+        case 'p':
+            text = input('Post Inhalt:\n')
+            response = None
+            try:
+                response = requests.post('http://192.168.6.129:8080', data=jsonpickle.encode(ClientPost(USERID, text)))
+            except requests.ConnectionError as err:
+                print('Server Error:\n' + err)
+                continue
+
+            if response.status_code == 200:
+                post = jsonpickle.decode(response.text)
+                print(f'{post.userID}\n{post.text}\nLikes: {post.likes}')
+            else:
+                print('ErrorStatusCode = ', response.status_code)

971fda03d44177df16e2e046e38315ef9d942ee2
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2023-05-25 13:36:59 +0200

    Request data validation

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ import mysql.connector
 from post import Post
 
 hostName = "192.168.6.179"
-mysqlhostname='192.168.6.179'
+mysqlhostname = '192.168.6.179'
 serverPort = 8080
 
 
@@ -21,6 +21,19 @@ def connect_to_db():
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        # Verarbeitet die Request Daten
+        content_length = int(self.headers['Content-Length'])
+        request_data = self.rfile.read(content_length)
+        try:
+            index = request_data.split()[0]
+            amount = request_data.split()[1]
+        except:
+            self.send_response(400)
+            self.send_header("Content-type", "text/text")
+            self.end_headers()
+            self.wfile.write(bytes('Malformed request','utf-8'))
+            return
+
         db = connect_to_db()
         self.send_response(200)
         self.send_header("Content-type", "text/json")
@@ -30,9 +43,19 @@ class MyServer(BaseHTTPRequestHandler):
 
     def do_POST(self):
         db = connect_to_db()
-        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-        post_data = self.rfile.read(content_length)
-        client_post = jsonpickle.decode(post_data)
+
+        # Verarbeitet die Request Daten
+        try:
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            client_post = jsonpickle.decode(post_data)
+        except:
+            self.send_response(400)
+            self.send_header("Content-type", "text/text")
+            self.end_headers()
+            self.wfile.write(bytes('Malformed request','utf-8'))
+            return
+
         try:
             password = db.cursor().execute(f"SELECT password FROM users WHERE userName=%s", (client_post.userName,))
         except mysql.connector.Error:
@@ -40,24 +63,28 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/text")
             self.end_headers()
             self.wfile.write(bytes('Falscher User oder Passwort!', 'utf-8'))
+            return
 
         if password == client_post.password:
             self.send_response(400)
             self.send_header("Content-type", "text/text")
             self.end_headers()
             self.wfile.write(bytes('Falscher User oder Passwort!', 'utf-8'))
+
         elif client_post.text != '':
             self.send_response(200)
             self.send_header("Content-type", "text/text")
             self.end_headers()
             self.wfile.write(bytes('Post gesendet!', 'utf-8'))
-            text=client_post.text
-            username=client_post.userName
+
+            text = client_post.text
+            username = client_post.userName
             print(username, ":\n", text)
-            userID=db.cursor().execute(f"SELECT userID FROM users WHERE userName=%s", (client_post.userName,))
+            userID = db.cursor().execute(f"SELECT userID FROM users WHERE userName=%s", (client_post.userName,))
             print(userID)
             db.curser().execute(f"INSERT INTO posts (userID, text, likes) VALUES (%s, %s, 0)", (userID, text))
             db.commit()
+
         else:
             self.send_response(400)
             self.send_header("Content-type", "text/text")

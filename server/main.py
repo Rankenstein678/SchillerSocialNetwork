f63518044d944c5b850b29e2d04dece1762b6810
PhillipZazzetta <zphillip98@gmail.com>
2023-07-18 09:08:31 +0200

    cors

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 import server.databank
 from models.LoginDataModel import LoginDataModel
@@ -13,6 +14,20 @@ app = FastAPI(
     docs_url="/",
 )
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/posts")
 def get_new_posts(amount: int | None = 10, after: int | None = None) -> list[S2CPostModel]:
